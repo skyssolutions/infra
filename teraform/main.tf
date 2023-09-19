b6a5661930c6c10a596ef265398a59b6fcf5a4ad
Skyler Mäntysaari <samip5@users.noreply.github.com>
2023-09-19 13:10:06 +0300

    Terraform things

---
+++ teraform/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    zitadel = {
+      source  = "zitadel/zitadel"
+      version = "1.0.2"
+    }
+  }
+}
+
+provider "zitadel" {
+  domain           = "auth.samipsolutions.fi"
+  insecure         = "true"
+  port             = "443"
+  token = "token.json"
+}
+
+
+resource "zitadel_org" "org" {
+  name = "tmi"
+}
+
+resource "zitadel_human_user" "default" {
+  org_id             = zitadel_org.org.id
+  user_name          = "sm"
+  first_name         = "Skyler"
+  last_name          = "Mäntysaari"
+  email              = "sm@samipsolutions.fi"
+  initial_password   = "Password1!"
+}
