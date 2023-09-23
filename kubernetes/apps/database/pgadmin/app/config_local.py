b36018e0a5d43cba1c6d4e4d9d1d078cb5d04666
Skyler Mäntysaari <samip5@users.noreply.github.com>
2023-09-23 11:27:12 +0300

    pgadmin with idp

--- kubernetes/apps/database/pgadmin/app/config_local.py
+++ kubernetes/apps/database/pgadmin/app/config_local.py
@@ -1,1 +1,18 @@
+AUTHENTICATION_SOURCES = ['oauth2']
+OAUTH2_AUTO_CREATE_USER = True
 MASTER_PASSWORD_REQUIRED = False
+
+OAUTH2_CONFIG = [{
+  'OAUTH2_NAME' : 'Zitadel',
+  'OAUTH2_DISPLAY_NAME' : 'IdP',
+  'OAUTH2_CLIENT_ID' : '${PGADMIN_OAUTH_CLIENT_ID}',
+  'OAUTH2_CLIENT_SECRET' : '${PGADMIN_OAUTH_SECRET}',
+  'OAUTH2_TOKEN_URL' : 'https://auth.${SECRET_DOMAIN}/oauth/v2/token',
+  'OAUTH2_AUTHORIZATION_URL' : 'https://auth.${SECRET_DOMAIN}/oauth/v2/authorize',
+  'OAUTH2_API_BASE_URL' : 'https://auth.${SECRET_DOMAIN}/',
+  'OAUTH2_USERINFO_ENDPOINT' : 'https://auth.${SECRET_DOMAIN}/oidc/v1/userinfo',
+  'OAUTH2_SERVER_METADATA_URL' : 'https://auth.${SECRET_DOMAIN}/.well-known/openid-configuration',
+  'OAUTH2_SCOPE' : 'openid email profile groups',
+  'OAUTH2_ICON' : 'fa-openid',
+  'OAUTH2_BUTTON_COLOR' : '#2db1fd'
+}]
